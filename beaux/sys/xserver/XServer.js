4cd41e86dc1a26755efa6c8c0a126d0ab4672faa
unisgn <unisgn@gmail.com>
2013-08-01 23:41:16 +0800

    Update XServer.js

--- beaux/sys/xserver/XServer.js
+++ beaux/sys/xserver/XServer.js
@@ -1,22 +1,44 @@
 Ext.define('Beaux.sys.xserver.XServer', {
     singleton: true,
     requires: [
-  
+  		'Ext.container.Viewport',
     ],
+    
+    /**
+     * @private
+     * @property
+     */
     windowManager: null,
+    
+    /**
+     * @private
+     * @property
+     */
 	viewport: null,
+	
+	/**
+	 * @private
+	 * @property
+	 */
 	rootXWindow: null,
 
+	/**
+	 * @private method
+	 */
     setRootXWindow: function(win) {
 		this.rootXWindow = win;
 	},
 	
-
+	/**
+	 * @public interface
+	 */
 	getRootXWindow: function() {
 		return this.rootXWindow;
 	},
 
-
+	/**
+	 * @public interface
+	 */
 	resetRootXWindow: function(win) {
 		var me = this;
 		if(me.rootXWindow) { 
@@ -28,7 +50,9 @@ Ext.define('Beaux.sys.xserver.XServer', {
 		me.rootXWindow.show();
 	},
 	
-
+	/**
+	 * @public interface
+	 */
     main: function() {
 		var me = this;
 		me.viewport = Ext.create('Ext.container.Viewport', {layout: 'fit'});
