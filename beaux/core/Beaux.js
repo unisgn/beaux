17b33a1cffac93d0371fb58768d11c2ca7c7e577
0xFranCiS <unisgn@gmail.com>
2016-05-07 23:45:59 +0800

    refactorary

--- beaux/core/Beaux.js
+++ beaux/core/Beaux.js
@@ -1,29 +1,65 @@
-Ext.define('Beaux.core.Beaux', {
-    singleton: true,
-    requires: [
-        'Beaux.sys.login.LoginManager',
-        'Beaux.sys.desktop.Cassie',
-        'Beaux.sys.xserver.XServer',
-        'Beaux.sys.application.ProcessManager'
-    ],
-    xserver: null,
-    loginManager : null,
-    
-    setLoginManager : function(loginManager) {
-        this.loginManager = loginManager;
-    },
-    getLoginManager : function() {
-        return this.loginManager;
-    },
-    
-    main: function() {
-        console.log('start beaux;');
-        Beaux.sys.xserver.XServer.main(); // prepare Xserver
+;(function () {
+    /**
+     * @private
+     * @type {Beaux.sys.lib.DesktopEnvironment}
+     */
+    var de;
 
-        
-        //Beaux.sys.login.LoginManager.main(); // launch login mgr
+    /**
+     * private
+     */
+    var dm;
 
-        Beaux.sys.desktop.Cassie.main(); 
-        console.log('beaux is ready;');
+    /**
+     * @private
+     * @type {Beaux.core.ProcMgr}
+     */
+    var processMgr = Ext.create('Beaux.core.ProcMgr');
+
+    /**
+     *
+     * @returns {Beaux.sys.lib.DesktopEnvironment}
+     */
+    Beaux.getDesktopManager = function () {
+        return de;
+    };
+
+    /**
+     *
+     * @param {Beaux.sys.lib.DesktopEnvironment} desktopMgr
+     */
+    Beaux.setDesktopManager = function (desktopMgr) {
+        de = desktopMgr;
+    };
+
+    Beaux.getDisplayManager = function () {
+        return dm;
+    };
+
+    Beaux.setDisplayManager = function (displayMgr) {
+        dm = displayMgr;
+    };
+
+    Beaux.getProcessMgr = function () {
+        return processMgr;
+    };
+
+    /**
+     * @public
+     */
+    Beaux.boot = function () {
+        Ext.log('start beaux boot;');
+
+        Beaux.sys.lib.XServer.main();
+
+
+        if (dm) {
+            dm.main();
+        } else if (de) {
+            de.main();
+        }
+
+        Ext.log('beaux loaded;');
     }
-});
+
+}());
